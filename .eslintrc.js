eefb8c4c4b787fffd9b30737d519940e66b82b94
Zt0 <azat.antonyan@tumo.org>
2023-08-23 16:39:01 +0400

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: ['node_modules'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 13,
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,15 +1,8 @@
+import React from 'react';
 import { StyleSheet, Image, ImageSourcePropType } from 'react-native';
 
-export default function ImageViewer({placeholderImageSource}: {placeholderImageSource: ImageSourcePropType}) {
-  return (
-    <Image source={placeholderImageSource} style={styles.image} />
-  );
-}
+const styles = StyleSheet.create({ image: { width: 30, height: 30, borderRadius: 18 } });
 
-const styles = StyleSheet.create({
-  image: {
-    width: 30,
-    height: 30,
-    borderRadius: 18,
-  },
-});
+export default function ImageViewer({ placeholderImageSource }: { placeholderImageSource: ImageSourcePropType }) {
+  return <Image source={placeholderImageSource} style={styles.image} />;
+}
